649ec7a9667f8301646f0ab45c19a5148d7e17a0
camicodes-fisagrp <camila.cedeno@fisagrp.com>
2023-08-17 00:07:37 -0500

    Primer Commit

--- src/com/javacodingexercises/set10/DaysOfWeek.java
+++ src/com/javacodingexercises/set10/DaysOfWeek.java
@@ -7,23 +7,18 @@ public enum DaysOfWeek {
     FRIDAY("Friday"),
     SATURDAY("Saturday"),
     SUNDAY("Sunday");
-
     private String name;
-
     DaysOfWeek(String name) {
         this.name = name;
     }
-
     public String getName() {
         // Write your code here to return the name of the day
         return this.name;
     }
-
     public boolean isWeekday() {
         // Write your code here to check if this day is a weekday
         return !isHoliday();
     }
-
     public boolean isHoliday() {
         // Write your code here to check if this day is a holiday
         //return this!=SATURDAY && this!=SUNDAY;
